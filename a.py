81a8ab3bb9e153dc16a90635421d3bcb8cb04b64
Connor Chen <cjc353@cornell.edu>
2022-03-05 12:49:47 -0500

    Made a feature for a.py

--- a.py
+++ a.py
@@ -1,7 +1,9 @@
 some_list = [1,2,3]
 
 def some_func(some_list):
-  some_list.append(4)
+  some_list.append(4, 5)
+  print("YEET")
+  some_list.append(5, 9, 12)
   print(some_list)
 
 some_func(some_list)
